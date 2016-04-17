a41ea57297ef41fe1a8d365d274438612b996d1f
Sam Sulaimanov <sam@bitmorse.com>
2016-04-17 04:56:02 +0200

    adding jsgraph

---
+++ README.md
@@ -0,0 +1,2 @@
+# axon-gps
+Example nervousnet axon that displays a map with the users current location.
---
+++ axon.js
@@ -0,0 +1,45 @@
+(function(){
+
+	//call the UI page "home"
+	App.load('home');
+
+		(function ( domGraph ) {
+
+			var graphinstance = new Graph( domGraph );
+
+			var data = {
+			  x: 0.5,
+			  dx: 0.2,
+			  y: []
+			};
+			for ( var i = 1, l = 30; i < l; i += 1 ) {
+			  data.y.push( Math.sin( i * 0.2 + 0.5 ) );
+			}
+
+			graphinstance.newSerie( "serieTest" )
+			  .setLabel( "My serie" )
+			  .autoAxis()
+			  .setData( data )
+			  .setMarkers();
+
+			graphinstance.redraw();
+			graphinstance.drawSeries();
+
+
+
+			})( "graph" );
+
+	//poll gyro
+	setInterval(function(){
+
+		$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/Gyroscope", function( data ) {
+
+
+
+		});
+
+
+	}, 1000);
+
+
+})();
